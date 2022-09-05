9301fecc825c6609d8c08015f4a30659d9c05ef2
alanlordsongit config --global user.email <alanlordson10@gmail.com>
2022-09-05 11:51:05 +0530

    hey angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { OneComponent } from './one/one.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,MatCardModule,MatIconModule,MatSidenavModule,
+    MatFormFieldModule,MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
